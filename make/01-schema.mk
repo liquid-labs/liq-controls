6b32f45ef07c795daa9a926d12293eae4e7a1717
Zane Rockenbaugh <zane@liquid-labs.com>
2023-06-08 11:37:25 -0500

    update makefile for new schema name

--- make/01-schema.mk
+++ make/01-schema.mk
@@ -1,5 +1,5 @@
 SCHEMA_SRC:=$(SRC)/schema
-SCHEMA_FILES:=$(SCHEMA_SRC)/controls.schema.json
+SCHEMA_FILES:=$(SCHEMA_SRC)/audit.schema.json
 SCHEMA_DIST:=$(patsubst $(SCHEMA_SRC)%, $(DIST)%, $(SCHEMA_FILES))
 
 $(SCHEMA_DIST): $(DIST)% : $(SCHEMA_SRC)%
