7cf647cf6903c5e0b9e807fe7e22986d9f6aca6a
Zane Rockenbaugh <zane@liquid-labs.com>
2024-09-14 15:48:21 -0500

    support general orgKey override from package.json:_comply.orgKey

--- src/lib/integrations/get-question-controls.mjs
+++ src/lib/integrations/get-question-controls.mjs
@@ -9,7 +9,7 @@ const getQuestionControls = async({ app, controlsName, projectName, reporter }) 
   const packageControlsPath = fsPath.join(projectPath, 'controls', controlsName + '.qcontrols.yaml')
   let controlsContent
   // load controlsContent
-  reporter.log(`attempting to load '${controlsName}' controls from: ${packageControlsPath}...`)
+  reporter.log(`Attempting to load '${controlsName}' controls from: ${packageControlsPath}...`)
   try {
     controlsContent = await fs.readFile(packageControlsPath, { encoding : 'utf8' })
     reporter.log('  success')
@@ -22,19 +22,28 @@ const getQuestionControls = async({ app, controlsName, projectName, reporter }) 
       throw e
     } // else, no problem, just doesn't define the control at the package level
     reporter.log('  not found')
-    let [orgKey, basename] = projectName.split('/')
-    if (basename === undefined) {
-      // then we have an unscoped project and we look at the package.json to determine the org key
-      orgKey = packageJSON._comply?.orgKey
-      if (orgKey === undefined) {
+
+    let orgKey = packageJSON._comply?.orgKey
+    if (orgKey !== undefined) {
+      reporter.log(`Found override orgKey '${orgKey}' in 'package.json:_comply.orgKey'...\n`)
+    }
+    else {
+      let basename
+      ;[orgKey, basename] = projectName.split('/')
+      if (basename === undefined) { // three is no override and we can't determine the orgKey from the package name
         throw new Error("Did not find expected '_comply.orgKey' in the 'package.json' file. This value must be defined for unscoped packages.")
       }
     }
+
     const org = app.ext._liqOrgs.orgs[orgKey]
     if (org !== undefined) {
+      // the 'controls' ItemManager is bound to the org by this plugin 'setup' and loadControls'.
       const controlsSpec = org.controls.getControl(controlsName)
 
       return controlsSpec
+    }
+    else {
+      reporter.log(`Did not find 'org' data for '${orgKey}'. Consider creating custom '${controlsName}' controls in package (${packageControlsPath}) or set 'package.json:_comply.orgKey' to specify an existing org.`)
     }
   } // controlsContent load section
 
